f0768941fd2812185f9c13dc207c15088f0edf0b
Galend <galend@mail.ru>
2020-09-01 14:22:54 +0300

    Init

---
+++ build/build.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "build" {
+  ami = "ami-0bcc094591f354be2"
+  instance_type = "t2.micro"
+  key_name = "ansible"
+  security_groups = ["sg-02cc480bb425000f4"]
+  subnet_id = "subnet-8f7f0ec2"
+  associate_public_ip_address = true
+  tags = {
+    Name = "build"
+  }
+
+  provisioner "local-exec" {
+    command = "sleep 120; ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u ubuntu --private-key ~/.ssh/ansible.pem -i '${aws_instance.build.public_ip},' build.yml"
+  }
+}
